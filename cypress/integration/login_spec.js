b83367e2095356bbfa92cc08a1c9c361cdddbf10
cynthia727 <cynthia.sy727@gmail.com>
2021-07-10 22:30:40 +1200

    add
    add login_spec

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -5,7 +5,9 @@ describe('login test suite', () => {
     
     it('happy path test', () => {
         cy.visit('http://saucedemo.com')
-
+        cy.get('[data-test=username').type('standard_user')
+        cy.get('[data-test=password]').type('secret_sauce')
+        cy.get('[data-test=login-button').click()
     })
 })
 
