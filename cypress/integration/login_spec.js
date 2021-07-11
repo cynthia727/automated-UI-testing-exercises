7c088bbf7bd85fb2cd13caff50af79998d8ab7a2
cynthia727 <cynthia.sy727@gmail.com>
2021-07-11 13:28:12 +1200

    Update login_spec.js
    Add "a bad password" test

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -8,6 +8,16 @@ describe('login test suite', () => {
         cy.get('[data-test=username').type('standard_user')
         cy.get('[data-test=password]').type('secret_sauce')
         cy.get('[data-test=login-button').click()
+    });
+
+    it('a bad password', () => {
+        cy.visit('http://saucedemo.com')
+        cy.get('[data-test=username').type('standard_user')
+        cy.get('[data-test=password]').type('abadpassword')
+        cy.get('[data-test=login-button').click()
+
+        cy.get('[data-test=error]').contains('password do not match any user in this service')
+        
     })
 })
 
